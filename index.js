11aca7769f8e404c3693e8d24828dca1e310d66f
Marc Roland Soniega <marcrolandmarc@gmail.com>
2024-03-27 12:03:14 +0800

    changes on hp bar

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var btnClick = document.getElementById('btn').addEventListener('click',function(
     if(counter < 1){
         counter = 0;
     }
+    setTimeout(function(){hpBar.style.backgroundColor = "green";},200);
     hpBar.style.width = counter + "%";
+    hpBar.style.backgroundColor = "red";
     console.log(counter + " Damage taken: " + RandomNumber);
 });
